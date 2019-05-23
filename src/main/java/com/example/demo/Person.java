adec85ed65b6f8e3cab8b87c6c577d4082702e5a
amyalmaw <amyalmaw@yahoo.com>
2019-05-23 02:00:05 -0400

    Ex_8 persistence to database

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+
+
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  long id;
+    private String firstName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+}
